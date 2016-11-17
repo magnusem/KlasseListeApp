3ed8976e42bae429ac510ddd85c5d4c19015b820
Magnus Emil Mortensen <magn1596@edu.easj.dk>
2016-11-17 11:23:01 +0100

    SelectedElev

--- KlasseListeApp/ViewModel/KlasseViewModel.cs
+++ KlasseListeApp/ViewModel/KlasseViewModel.cs
@@ -9,6 +9,16 @@ namespace KlasseListeApp.ViewModel
     class KlasseViewModel
     {
         public Model.KlasseListe KListe { get; set; }
+
+        private Model.KlasseInfo SelectedElev;
+
+        public Model.KlasseInfo selectedElev
+        {
+            get { return SelectedElev; }
+            set { SelectedElev = value; }
+        }
+
+
         public KlasseViewModel()
         {
             KListe = new Model.KlasseListe();
